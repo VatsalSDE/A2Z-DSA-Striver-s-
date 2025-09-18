8631482e5784ad99655054ca9bf7bf914f88166a
Vatsal Deepak Rathi <ds099315@gmail.com>
2025-09-18 12:46:48 +0530

    fibonacci series

--- Step 1/Basic Recursion/fibona.java
+++ Step 1/Basic Recursion/fibona.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 
 public class fibona {
-    private static void fibo(int n){
-        // recursive code for this 
+    private static int fibo(int n){
+        // recursive code for this
+        if(n<=1){
+            return n;
+        }
+        int last = fibo(n-1);
+        int second=fibo(n-2); 
+
+        return last + second;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the terms of fibonaci you want");
-        int n = sc .nextInt();
-        fibo(n);    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the terms of Fibonacci you want");
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+        System.out.print(fibo(i) + " ");
+        }
     }
 }
